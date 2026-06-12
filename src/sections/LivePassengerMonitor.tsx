d8dfa5ed8e65894f9e48d3a5fd4a5f32f3f55553
salmanf.rahman <salmanfarid43@gmail.com>
2026-06-12 20:05:50 +0600

    video added...

--- src/sections/LivePassengerMonitor.tsx
+++ src/sections/LivePassengerMonitor.tsx
@@ -157,7 +157,7 @@ const LivePassengerMonitor = memo(function LivePassengerMonitor({
               animate={{ opacity: 1, x: 0 }}
               exit={{ opacity: 0, x: 20 }}
               transition={{ duration: 0.7, ease: [0.16, 1, 0.3, 1] }}
-              className="w-64 border-l border-slate-200 bg-white p-3 overflow-y-auto relative"
+              className="w-64 border-l border-slate-200 bg-white p-3 pb-10 overflow-y-auto relative"
               style={{ maxHeight: '340px' }}
             >
               <button
@@ -179,7 +179,7 @@ const LivePassengerMonitor = memo(function LivePassengerMonitor({
               </div>
 
               {/* Camera feed */}
-              <div className="relative w-full h-28 rounded border border-slate-200 mb-3 overflow-hidden bg-black">
+              <div className="relative w-full h-32 rounded border border-slate-200 mb-3 overflow-hidden bg-black">
                 <iframe
                   id="passenger-feed-video"
                   src="https://www.youtube.com/embed/q83bhmzABGE?autoplay=1&mute=1&loop=1&playlist=q83bhmzABGE&controls=0&showinfo=0&rel=0&iv_load_policy=3&modestbranding=1&playsinline=1&fs=0&cc_load_policy=0&enablejsapi=1"
